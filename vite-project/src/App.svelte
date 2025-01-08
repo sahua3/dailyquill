677c07d1c26b4abe7bca872eb3842fe30465f8e4
sahua3 <sahua3@vcu.edu>
2025-01-08 17:58:06 -0500

    removed login.svelte to redo it, altered vite-env to ensure my router is imported in app

--- vite-project/src/App.svelte
+++ vite-project/src/App.svelte
@@ -1,14 +1,8 @@
 <script>
-  import Router from 'svelte-spa-router';
+  import { Router } from 'svelte-spa-router';
   import routes from './routes.js';
 </script>
 
 <main>
   <Router {routes} />
 </main>
-
-<style>
-  main {
-    padding: 20px;
-  }
-</style>
