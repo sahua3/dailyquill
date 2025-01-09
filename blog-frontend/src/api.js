90f15d999c2209e061566836a5e8310b3cf1eb51
sahua3 <sahua3@vcu.edu>
2025-01-08 21:14:58 -0500

    i ran into problems!!!!! going to create a fresh front end to try and resolve

--- blog-frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
-});
-
-export const getPosts = () => api.get('/posts');
-export const createPost = (post) => api.post('/posts', post);
---
+++ vite-project/src/components/main.svelte
@@ -0,0 +1,11 @@
+<style> 
+    div {
+        background-color: pink;
+    }
+</style>
+
+<main>
+    <div>
+        <h1> Hello </h1>
+    </div>
+</main>
